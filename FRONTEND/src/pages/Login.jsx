515a2afc4b5971e20c4601dd8613ce72a87781e5
st10365209 <126856472+Letlotlo-Motloung@users.noreply.github.com>
2025-10-10 22:39:54 +0200

    Login.jsx--reference

--- FRONTEND/src/pages/Login.jsx
+++ FRONTEND/src/pages/Login.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Helmet } from 'react-helmet-async'
-import LoginForm from '../components/Forms/LoginForm'
+import React from 'react'; // Import React to create components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { Helmet } from 'react-helmet-async'; // Import Helmet for managing document head (title, meta tags, etc.)
+import LoginForm from '../components/Forms/LoginForm'; // Import the LoginForm component which handles user login
 
+// Login component definition
 const Login = () => {
     return (
         <>
+            {/* Helmet component used to modify the head of the document */}
             <Helmet>
-                <title>Login - International Payments Portal</title>
-                <meta name="description" content="Login to your international payments account" />
+                <title>Login - International Payments Portal</title> {/* Sets the title for the page */}
+                <meta name="description" content="Login to your international payments account" /> {/* Sets meta description for SEO */}
             </Helmet>
+
+            {/* Render the LoginForm component, which will contain the form logic for logging in */}
             <LoginForm />
         </>
     )
 }
 
-export default Login
+export default Login; // Export the Login component for use in other parts of the application
